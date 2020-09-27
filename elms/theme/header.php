1d053aac5a98572f908e5d95062ced6cae2dd402
Raghu <raghunath@adrobit.com>
2020-09-27 13:39:57 +0530

    fix

--- elms/theme/header.php
+++ elms/theme/header.php
@@ -2,7 +2,7 @@
 <header class="default-header">
 		<nav class="navbar navbar-expand-lg  navbar-light">
 			<div class="container">
-				<a class="navbar-brand" href="index" style="color:white">LMS</a>
+				<a class="navbar-brand" href="" style="color:white">LMS</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 				 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="lnr lnr-menu"></span>
